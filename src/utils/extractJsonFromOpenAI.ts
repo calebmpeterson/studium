e1fc66655eb3c834f20dcb3d9c4fc4553e1bd52c
caleb <caleb.peterson@cubicle6.com>
2023-12-14 19:03:54 -0500

    handle place extraction JSON parsing errors more verbosely

--- src/utils/extractJsonFromOpenAI.ts
+++ src/utils/extractJsonFromOpenAI.ts
@@ -1,16 +1,21 @@
+import { get } from "lodash";
 import { OpenAI } from "openai";
 
 export const extractJsonFromOpenAI = (response: OpenAI.ChatCompletion) => {
-  try {
-    if (response.choices[0]) {
-      const { content } = response.choices[0].message;
+  if (response.choices[0]) {
+    const { content } = response.choices[0].message;
 
-      if (content) {
+    if (content) {
+      try {
         return JSON.parse(content);
+      } catch (error: unknown) {
+        console.error(
+          `Failed to extract JSON from OpenAI response`,
+          get(error, "message", "Unknown error"),
+          content
+        );
       }
     }
-  } catch (error: unknown) {
-    console.error(`Failed to extract JSON from OpenAI response`, error);
   }
 
   return null;
