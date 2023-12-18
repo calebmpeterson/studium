3107eb0a17dad077943099d9a17d62001f8a26b4
caleb <caleb.peterson@cubicle6.com>
2023-12-18 08:05:17 -0500

    extract CrossReferencesDisplay component

---
+++ src/components/CrossReferencesDisplay.tsx
@@ -0,0 +1,113 @@
+import { shadows } from "@/styles/shadows";
+import { CrossReference, Verse } from "@/types";
+import { css } from "@emotion/react";
+import { mdiClose } from "@mdi/js";
+import Icon from "@mdi/react";
+import Link from "next/link";
+import { FC } from "react";
+
+const crossReferencesBackgroundCss = css`
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  backdrop-filter: blur(2px);
+`;
+
+const crossReferencesContainerCss = css`
+  position: fixed;
+  z-index: 1;
+  bottom: 0px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  box-sizing: border-box;
+  max-width: 800px;
+  border: 1px solid var(--border-color);
+  border-bottom: none;
+  padding: 20px 20px 0;
+  border-radius: 5px 5px 0 0;
+  background-color: var(--bg);
+  max-height: 80vh;
+
+  box-shadow: ${shadows["shadow-xl"]};
+`;
+
+const crossReferencesBodyCss = css`
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding-bottom: 20px;
+`;
+
+const crossReferencesHeaderCss = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const crossReferenceCss = css`
+  color: ;
+`;
+
+interface Props {
+  book: string;
+  chapter: string;
+  verse: string;
+  crossReferences: CrossReference[];
+  onClose: () => void;
+}
+
+export const CrossReferencesDisplay: FC<Props> = ({
+  book,
+  chapter,
+  verse,
+  crossReferences,
+  onClose,
+}) => (
+  <>
+    <div css={crossReferencesBackgroundCss} />
+    <div css={crossReferencesContainerCss}>
+      <div css={crossReferencesHeaderCss}>
+        <strong data-muted>
+          <small>
+            {book} {chapter}:{verse} Cross References
+          </small>
+        </strong>
+
+        <button
+          role="button"
+          aria-label="Close cross references"
+          data-icon
+          onClick={onClose}
+        >
+          <Icon path={mdiClose} size={0.7} />
+        </button>
+      </div>
+
+      <div css={crossReferencesBodyCss}>
+        {crossReferences.map((crossReference) => (
+          <div key={crossReference.slug}>
+            {crossReference.text ?? (
+              <em data-muted>Failed to look up cross reference text.</em>
+            )}
+            <div>
+              <small data-muted css={crossReferenceCss}>
+                <Link href={crossReference.slug}>
+                  {crossReference.book} {crossReference.chapter}:
+                  {crossReference.verse}
+                </Link>
+              </small>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </>
+);
--- src/components/VerseDisplay.tsx
+++ src/components/VerseDisplay.tsx
@@ -1,11 +1,8 @@
-import { shadows } from "@/styles/shadows";
 import { CrossReference, Verse } from "@/types";
 import { css } from "@emotion/react";
-import { mdiClose } from "@mdi/js";
-import Icon from "@mdi/react";
 import { isEmpty } from "lodash";
-import Link from "next/link";
 import { FC, useCallback, useState } from "react";
+import { CrossReferencesDisplay } from "./CrossReferencesDisplay";
 
 const containerCss = css`
   position: relative;
@@ -14,56 +11,6 @@ const containerCss = css`
 const anchorCss = css`
   position: absolute;
   top: -50vh;
-`;
-
-const crossReferencesBackgroundCss = css`
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  backdrop-filter: blur(2px);
-`;
-
-const crossReferencesContainerCss = css`
-  position: fixed;
-  z-index: 1;
-  bottom: 0px;
-  left: 0;
-  right: 0;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  box-sizing: border-box;
-  max-width: 800px;
-  border: 1px solid var(--border-color);
-  border-bottom: none;
-  padding: 20px 20px 0;
-  border-radius: 5px 5px 0 0;
-  background-color: var(--bg);
-  max-height: 80vh;
-
-  box-shadow: ${shadows["shadow-xl"]};
-`;
-
-const crossReferencesBodyCss = css`
-  overflow: auto;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  padding-bottom: 20px;
-`;
-
-const crossReferencesHeaderCss = css`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const crossReferenceCss = css`
-  color: ;
 `;
 
 interface Props extends Verse {
@@ -84,6 +31,10 @@ export const VerseDisplay: FC<Props> = ({
     setAreCrossReferencesOpen((open) => !open);
   }, []);
 
+  const onCloseCrossReferences = useCallback(() => {
+    setAreCrossReferencesOpen(false);
+  }, []);
+
   return (
     <div css={containerCss}>
       <div id={verse} css={anchorCss} />
@@ -99,47 +50,13 @@ export const VerseDisplay: FC<Props> = ({
             Cross refs
           </a>
           {areCrossReferencesOpen && (
-            <>
-              <div css={crossReferencesBackgroundCss} />
-              <div css={crossReferencesContainerCss}>
-                <div css={crossReferencesHeaderCss}>
-                  <strong data-muted>
-                    <small>
-                      {book} {chapter}:{verse} Cross References
-                    </small>
-                  </strong>
-
-                  <button
-                    role="button"
-                    aria-label="Close cross references"
-                    data-icon
-                    onClick={onToggleCrossReferences}
-                  >
-                    <Icon path={mdiClose} size={0.7} />
-                  </button>
-                </div>
-
-                <div css={crossReferencesBodyCss}>
-                  {crossReferences.map((crossReference) => (
-                    <div key={crossReference.slug}>
-                      {crossReference.text ?? (
-                        <em data-muted>
-                          Failed to look up cross reference text.
-                        </em>
-                      )}
-                      <div>
-                        <small data-muted css={crossReferenceCss}>
-                          <Link href={crossReference.slug}>
-                            {crossReference.book} {crossReference.chapter}:
-                            {crossReference.verse}
-                          </Link>
-                        </small>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </>
+            <CrossReferencesDisplay
+              book={book}
+              chapter={chapter}
+              verse={verse}
+              crossReferences={crossReferences}
+              onClose={onCloseCrossReferences}
+            />
           )}
         </>
       )}
