bd04ccfb05e0ec85b655f6f8fdd5be35990cef01
caleb <caleb.peterson@cubicle6.com>
2023-12-20 21:33:31 -0500

    fix double <Header> error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,11 +8,9 @@ export default function Document() {
       <Head>
         <Global styles={globalCss} />
 
-        <Head>
-          <meta name="description" content="Bible study" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <meta name="description" content="Bible study" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
