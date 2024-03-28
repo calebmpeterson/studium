d32f4b8cf2849b7c777a6d942c7243485ad02076
caleb <caleb.peterson@cubicle6.com>
2024-03-27 21:42:57 -0400

    handle /book/chapter:verse urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        // Redirect /book/chapter:verse to /book/chapter#verse
+        source: "/:book/:chapter\\::verse",
+        destination: "/:book/:chapter#:verse",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
