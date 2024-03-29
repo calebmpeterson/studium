a932abb033813d27dd2ef15b8a7f5b26440e0fe4
caleb <caleb.peterson@cubicle6.com>
2024-03-28 21:28:19 -0400

    /book/chapter:verse redirects not working - removing

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        // Redirect /book/chapter:verse to /book/chapter#verse
-        source: "/:book/:chapter\\::verse",
-        destination: "/:book/:chapter#:verse",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
