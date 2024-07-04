ce4a088b4782b302ec56d2e6bc36f4814f2c06d4
caleb <caleb.peterson@cubicle6.com>
2024-07-04 13:36:04 -0400

    drop experimental largePageDataBytes config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    largePageDataBytes: 1024 * 192, // 192kB
-  },
 };
 
 module.exports = nextConfig;
