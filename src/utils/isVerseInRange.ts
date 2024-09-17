3a306024d47add4e07557db022c9a772f52a138b
caleb <caleb.peterson@cubicle6.com>
2024-09-17 17:44:30 -0400

    handle multiple comma delimited highlight ranges

--- src/utils/isVerseInRange.ts
+++ src/utils/isVerseInRange.ts
@@ -1,12 +1,18 @@
 export const isVerseInRange = (verse: number, range: string): boolean => {
-  // Check if the range is a single number
-  if (!range.includes("-")) {
-    const single = parseInt(range, 10);
-    return verse === single;
-  }
+  // Split by comma to handle multiple ranges
+  const ranges = range.split(",");
 
-  // Split the range into min and max values
-  const [min, max] = range.split("-").map((val) => parseInt(val, 10));
+  // Check each range
+  return ranges.some((r) => {
+    // Check if the current range is a single number
+    if (!r.includes("-")) {
+      const singleNumber = parseInt(r.trim(), 10);
+      return verse === singleNumber;
+    }
 
-  return verse >= min && verse <= max;
+    // Split the range into min and max values
+    const [min, max] = r.split("-").map((val) => parseInt(val.trim(), 10));
+
+    return verse >= min && verse <= max;
+  });
 };
