aca03c7c8d432173445d3c26eef1e54a1047f026
Caleb Peterson <caleb.m.peterson@gmail.com>
2025-06-08 15:32:46 -0400

    Sentence case resources menu

--- src/data/resources.ts
+++ src/data/resources.ts
@@ -5,8 +5,8 @@ export interface ResourceLink {
 }
 
 export const RESOURCE_LINKS: ResourceLink[] = [
-  { label: "First Mention", href: "/first-mention", shallow: true },
-  { label: "Thompson Chain Reference", href: "/thompson-chain-reference", shallow: true },
+  { label: "First mention", href: "/first-mention", shallow: true },
+  { label: "Thompson chain reference", href: "/thompson-chain-reference", shallow: true },
   { label: "Timeline", href: "/timeline", shallow: true },
-  { label: "Source Code", href: "https://github.com/calebmpeterson/studium" },
+  { label: "Source code", href: "https://github.com/calebmpeterson/studium" },
 ];
